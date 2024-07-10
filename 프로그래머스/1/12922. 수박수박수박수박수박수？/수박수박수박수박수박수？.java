b7093ebe72f66920a9f197b1fc8c927b220f2962
juno0432 <129417004+juno0432@users.noreply.github.com>
2024-07-10 15:13:56 +0900

    Update 수박수박수박수박수박수？.java

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -1,11 +1,13 @@
 class Solution {
     public String solution(int n) {
+        StringBuilder result = new StringBuilder();
         String answer = "";
         for(int i = 1; i<=n; i++) {
             if (i % 2 != 0)
-                answer += "수";
-            else answer +="박";
+               result.append("수");
+            else result.append("박");
         }
+        answer=result.toString();
         return answer;
     }
 }
