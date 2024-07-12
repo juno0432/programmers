1890cc0b76d2e5bafddc0a0cce3df36b9b5e758c
juno0432 <129417004+juno0432@users.noreply.github.com>
2024-07-12 16:03:04 +0900

    Update 문자열 내림차순으로 배치하기.java

--- 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.java
+++ 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.java
@@ -3,17 +3,13 @@ class Solution {
     public String solution(String s) {
         StringBuilder sb = new StringBuilder();
         char[] arr = s.toCharArray();
-        int[] arrInt = new int[arr.length];
         
-        for (int i = 0; i < arr.length; i++) {
-            arrInt[i] = arr[i];
+        Arrays.sort(arr);
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+            sb.append((char) arr[i]);
         }
-        Arrays.sort(arrInt);
-        
-        for (int i = arrInt.length - 1; i >= 0; i--) {
-            sb.append((char) arrInt[i]);
-        }
-        
+
         String answer = sb.toString();
         return answer;
     }
