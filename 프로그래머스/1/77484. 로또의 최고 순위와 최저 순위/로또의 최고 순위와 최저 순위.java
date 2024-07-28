a1203f4e3f1cb8108905f0d93a722ce3c92d0946
juno0432 <129417004+juno0432@users.noreply.github.com>
2024-07-29 01:10:30 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.08 ms, Memory: 83.1 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
@@ -1,49 +1,39 @@
+import java.util.HashSet;
+import java.util.Set;
+
 class Solution {
     public int[] solution(int[] lottos, int[] win_nums) {
-        int[] answer = new int [2];
+        Set<Integer> winNumsSet = new HashSet<>();
+        for (int win_num : win_nums) {
+            winNumsSet.add(win_num);
+        }
+
         int countMax = 0;
         int countMin = 0;
-
-        for(int i=0;i<lottos.length;i++)
-        {
-            if(lottos[i]==0)
-            {
-                countMax++;
-            }
-            else
-            {
-                for(int j=0; j<win_nums.length; j++)
-                {
-                    if(lottos[i] == win_nums[j])
-                    {
-                        countMax++;
-                        countMin++;
-                    }
-                }
+        
+        for (int lotto : lottos) {
+            if (lotto == 0) {
+                countMax++;  // 0은 모르는 번호로, 최대 맞출 수 있는 경우로 계산
+            } else if (winNumsSet.contains(lotto)) {
+                countMax++;  // 맞춘 경우, 최대 맞춘 수도 증가
+                countMin++;  // 최소 맞춘 수도 증가
             }
         }
-        answer[0]=rank(countMax);
-        answer[1]=rank(countMin);
         
+        int[] answer = new int[2];
+        answer[0] = rank(countMax);  // 최대 순위
+        answer[1] = rank(countMin);  // 최소 순위
         return answer;
     }
 
-    private int rank(int count)
-    {
-        switch (count)
-        {
-            case 6:
-                return 1;
-            case 5 :
-                return 2;
-            case 4 :
-                return 3;
-            case 3 :
-                return 4;
-            case 2 :
-                return 5;
-            default:
-                return 6;
+    private int rank(int count) {
+        switch (count) {
+            case 6: return 1;
+            case 5: return 2;
+            case 4: return 3;
+            case 3: return 4;
+            case 2: return 5;
+            default: return 6;
         }
     }
 }
