a50f5e4d5ed856e2b666a4dc7cfaa771bf3da0aa
juno0432 <129417004+juno0432@users.noreply.github.com>
2024-08-12 09:13:26 +0900

    [level 1] Title: 덧칠하기, Time: 3.89 ms, Memory: 80.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -0,0 +1,22 @@
+class Solution {
+        public int solution(int n, int m, int[] section) {
+            int answer = 0;
+            int [] arr = new int [n];
+            for(int i = 0 ; i < section.length; i++)
+            {
+                arr[section[i]-1] = 1;
+            }
+            for(int i = 0 ; i < n; i++)
+            {
+                if(arr[i] == 1)
+                {
+                    for(int j = i ; j<i+m && j<arr.length; j++)
+                    {
+                        arr[j]=0;
+                    }
+                    answer++;
+                }
+            }
+            return answer;
+        }
+    }
