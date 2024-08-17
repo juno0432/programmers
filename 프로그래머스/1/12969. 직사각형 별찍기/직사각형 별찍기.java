416d012552b9432a601296282a1d11ef1f487fd3
juno0432 <129417004+juno0432@users.noreply.github.com>
2024-08-17 23:14:55 +0900

    [level 1] Title: 직사각형 별찍기, Time: 295.24 ms, Memory: 68 MB -BaekjoonHub

--- 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.java
+++ 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.java
@@ -1,18 +1,21 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
 class Solution {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        
-        for(int i=0 ; i<b; i++)
-        {
-            for(int j =0 ; j<a; j++)
-            {
-                System.out.print("*");
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] inputs = br.readLine().split(" ");
+        int a = Integer.parseInt(inputs[0]);
+        int b = Integer.parseInt(inputs[1]);
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < b; i++) {
+            for(int j = 0; j < a; j++) {
+                sb.append("*");
             }
-            System.out.println();
+            sb.append("\n");
         }
+        System.out.print(sb.toString());
     }
 }
